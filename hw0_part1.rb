d35f9867798cd95aac27b48500d66b37a3d51277
Thomas Ochman <thomas@nocebo.se>
2014-05-03 11:16:19 +0200

    made test pass and wrote a second test

--- hw0_part1.rb
+++ hw0_part1.rb
@@ -1,7 +1,8 @@
 # Define a method sum which takes an array of integers as an argument and returns the sum of its elements. For an empty array it should return zero.
 
 def sum(array)
-
+  return 0 if array.empty?
 end
 
 raise 'sum([]) != 0' unless sum([]) == 0
+raise 'sum([1, 2, 3]) != 6' unless sum([1, 2, 3]) == 6
