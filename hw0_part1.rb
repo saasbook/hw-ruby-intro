618b8c05cc6997a2932a67cf4e7c666c2f76ff8b
Sam Joseph <tansaku@gmail.com>
2014-05-03 06:10:00 -0300

    testing empty array returns zero

---
+++ hw0_part1.rb
@@ -0,0 +1,7 @@
+# Define a method sum which takes an array of integers as an argument and returns the sum of its elements. For an empty array it should return zero.
+
+def sum(array)
+
+end
+
+raise 'sum([]) != 0' unless sum([]) == 0
