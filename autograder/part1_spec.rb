a536c44152dcc89f8075832dcb2271eda2c87024
Juan Pinzon <juancpinzone@berkeley.edu>
2015-07-07 19:56:02 -0700

    Travis

--- autograder/part1_spec.rb
+++ autograder/part1_spec.rb
@@ -46,24 +46,22 @@ describe 'Ruby intro part 1' do
       expect(sum_to_n?([-1,-2,3,4,5,-8], 12)).to be false
       expect(sum_to_n?([-1,-2,3,4,6,-8], 12)).to be false
     end
+    #    for rspec 2.14.1
+    # it "returns false for the single element array [5 points]" do
+    #   sum_to_n?([1], 1).should be_false
+    #   sum_to_n?([3], 0).should be_false
+    # end
+    # it "returns false for the empty array [5 points]" do
+    #   sum_to_n?([], 0).should be_false
+    #   sum_to_n?([], 7).should be_false
+    # end
     it "returns false for the single element array [5 points]" do
-      sum_to_n?([1], 1).should be_false
-      sum_to_n?([3], 0).should be_false
+      expect(sum_to_n?([1], 1)).to be false
+      expect(sum_to_n?([3], 0)).to be false
     end
     it "returns false for the empty array [5 points]" do
-      sum_to_n?([], 0).should be_false
-      sum_to_n?([], 7).should be_false
+      expect(sum_to_n?([], 0)).to be false
+      expect(sum_to_n?([], 7)).to be false
     end
-
-    #
-    #for rspec 3.x
-    #it "returns false for the single element array [5 points]" do
-    #  expect(sum_to_n?([1], 1)).to be false
-    #  expect(sum_to_n?([3], 0)).to be false
-    #end
-    #it "returns false for the empty array [5 points]" do
-    #  expect(sum_to_n?([], 0)).to be false
-    #  expect(sum_to_n?([], 7)).to be false
-    #end
   end
 end
