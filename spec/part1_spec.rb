8305169d8de60c3a304ab4ef30bafeabf262f5ae
Liang Gong <JacksonGL@users.noreply.github.com>
2016-01-26 10:32:18 -0800

    update the spec to make it consistent with the auto-grader

--- spec/part1_spec.rb
+++ spec/part1_spec.rb
@@ -65,8 +65,8 @@ describe 'Ruby intro part 1' do
       expect(sum_to_n?([-1], -1)).to be false
       expect(sum_to_n?([-3], 0)).to be false
     end
-    it "returns true if the empty array sums to zero [5 points]" , points: 5 do
-      expect(sum_to_n?([], 0)).to be true
+    it "returns false for an empty array [5 points]" , points: 5 do
+      expect(sum_to_n?([], 0)).to be false
       expect(sum_to_n?([], 7)).to be false
     end
   end
