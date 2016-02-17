b9785a561c0f868e1877c20fd2e29722d606c92e
Maxwell Gove <mg3854@nyu.edu>
2016-02-17 04:11:42 +0000

    assignment 1

--- lib/ruby_intro.rb
+++ lib/ruby_intro.rb
@@ -3,33 +3,47 @@
 # Part 1
 
 def sum arr
-  # YOUR CODE HERE
+  arr.empty? ? 0 : arr.reduce(:+)
 end
 
 def max_2_sum arr
-  # YOUR CODE HERE
+  arr.empty? ? 0 : arr.length == 1 ? arr.first : arr.sort.last(2).reduce(:+)
 end
 
 def sum_to_n? arr, n
-  # YOUR CODE HERE
+  arr.combination(2).any? do |first, second|
+    first + second == n
+  end
 end
 
 # Part 2
 
 def hello(name)
-  # YOUR CODE HERE
+  "Hello, #{name}"
 end
 
 def starts_with_consonant? s
-  # YOUR CODE HERE
+  s.length > 0 && s !~ /^[AEIOU_\d\W]/i
 end
 
 def binary_multiple_of_4? s
-  # YOUR CODE HERE
+  s.length > 0 && s.to_i(2) % 4 == 0 && s !~ /^[:alpha]/
 end
 
 # Part 3
 
 class BookInStock
-# YOUR CODE HERE
+  
+  attr_accessor :isbn, :price
+
+  def initialize(isbn, price)
+    raise ArgumentError if isbn.empty? || price <= 0
+    @isbn = isbn
+    @price = price
+  end
+  
+  def price_as_string
+    "$%.2f" % price
+  end
+
 end
