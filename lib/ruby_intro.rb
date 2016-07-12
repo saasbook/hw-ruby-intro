9cdeba8fb21942f5ee3c2c8d5788ad8936818bb3
deepali-b <bajajdeepali0@gmail.com>
2016-07-12 11:55:52 +0530

    Update ruby_intro.rb

--- lib/ruby_intro.rb
+++ lib/ruby_intro.rb
@@ -4,20 +4,38 @@
 
 def sum arr
   # YOUR CODE HERE
+  if arr.length == 0 then
+ return 0
+else
+total  = 0
+arr.each { |a| total+=a }
+return total
+end
 end
 
 def max_2_sum arr
   # YOUR CODE HERE
+  case arr.length
+    when 0
+      return 0
+    when 1
+      return arr.first
+    else
+      return (arr.sort!.pop)+(arr.sort!.pop)
+  end
 end
 
 def sum_to_n? arr, n
   # YOUR CODE HERE
+  c_p = arr.product(arr).select! { |c| c[0]+c[1] == val }
+  not c_p.empty?
 end
 
 # Part 2
 
 def hello(name)
   # YOUR CODE HERE
+  return "Hello  "+name
 end
 
 def starts_with_consonant? s
