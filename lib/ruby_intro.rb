2f3b0ffc396b05e3cea04ba1bc2f8898a32f8ebc
xtrajacoby <ekstrand@grinnell.edu>
2016-08-31 12:21:11 -0500

    Update ruby_intro.rb

--- lib/ruby_intro.rb
+++ lib/ruby_intro.rb
@@ -3,33 +3,77 @@
 # Part 1
 
 def sum arr
-  # YOUR CODE HERE
+  sum = 0
+  arr.next {|x| sum += x}
 end
 
 def max_2_sum arr
-  # YOUR CODE HERE
+  if arr.length == 0
+    return 0
+    elsif arr.length == 1
+    return arr[0]
+  else
+    arr.sort! {|x, y| y <==> x}
+    return arr[0] + arr[1]
+  end
 end
 
 def sum_to_n? arr, n
-  # YOUR CODE HERE
+  arr.next {|x| arr.next{|y| return true if x + y = n}}
+end
+  return false
 end
 
 # Part 2
 
 def hello(name)
-  # YOUR CODE HERE
+  "Hello, " + name.to_s
 end
 
 def starts_with_consonant? s
-  # YOUR CODE HERE
+  if s =~ /\A(?=[^aeiou])(?=[a-z])/i
+    return true
+  else
+    return false
+  end
 end
 
 def binary_multiple_of_4? s
-  # YOUR CODE HERE
+  if s = ^[10]*00$
+    return true
+  else
+    return false
+  end
 end
 
 # Part 3
 
 class BookInStock
-# YOUR CODE HERE
+def initialize(isbn, price)
+  if isbn == "" || price <= 0
+    ArgumentError
+  end
+  @isbn = isbn
+  @price = price
+end
+
+def isbn
+  @isbn
+end
+def isbn=(x)
+  @isbn = x
+end
+
+def price=(x)
+  @price = x
+end
+def price
+  @price
+end
+
+def price_as_string
+  decimal = @price - @price.floor
+  "$#{@price.floor}#{decimal}"
+end
+
 end
