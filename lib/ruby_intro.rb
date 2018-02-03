abdace47318f4eab59a3f6ad6f997202dd755ee6
Efim Bogdanovsky <efim.bogdanovsky@gmail.com>
2018-02-03 20:05:02 +0000

    Complete homework

--- lib/ruby_intro.rb
+++ lib/ruby_intro.rb
@@ -2,34 +2,74 @@
 
 # Part 1
 
-def sum arr
-  # YOUR CODE HERE
+def sum(ary)
+  ary.sum
 end
 
-def max_2_sum arr
-  # YOUR CODE HERE
+def max_2_sum(ary)
+  return ary.max || 0 if ary.length < 2
+
+  pairs = ary.map.each_cons(2)
+  sums  = pairs.map(&:sum)
+
+  sums.max
 end
 
-def sum_to_n? arr, n
-  # YOUR CODE HERE
+def sum_to_n?(ary, n)
+  pairs = ary.map.each_cons(2)
+  sums  = pairs.map(&:sum)
+
+  sums.include?(n)
 end
 
 # Part 2
 
 def hello(name)
-  # YOUR CODE HERE
+  "Hello, #{name}"
 end
 
-def starts_with_consonant? s
-  # YOUR CODE HERE
+def letter?(s)
+  /[A-Za-z]+/ =~ s
 end
 
-def binary_multiple_of_4? s
-  # YOUR CODE HERE
+def starts_with_consonant?(s)
+  vowels  = 'aeoui'
+  initial = s.chars.first
+
+  letter?(initial) && !vowels.include?(initial.downcase)
+end
+
+def binary_multiple_of_4?(s)
+  rem = Integer(s, 2) % 4
+  rem.zero?
+rescue ArgumentError
+  false
 end
 
 # Part 3
 
 class BookInStock
-# YOUR CODE HERE
+  def initialize(isbn, price)
+    raise ArgumentError if !isbn_valid?(isbn)
+    raise ArgumentError if !price_valid?(price)
+
+    self.isbn  = isbn
+    self.price = price
+  end
+
+  def price_as_string
+    '$%.2f' % price
+  end
+
+  attr_accessor :isbn, :price
+
+  private
+
+  def isbn_valid?(isbn)
+    isbn.is_a?(String) && !isbn.empty?
+  end
+
+  def price_valid?(price)
+    (price.is_a?(Integer) || price.is_a?(Float)) && price.positive?
+  end
 end
