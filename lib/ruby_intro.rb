c6d5b74a70af5edc48d0abd8d3fd46149bf67852
Vini <vinicius.carriel@gmail.com>
2019-03-25 16:54:22 -0300

    First draft of the lib

--- lib/ruby_intro.rb
+++ lib/ruby_intro.rb
@@ -3,33 +3,65 @@
 # Part 1
 
 def sum arr
-  # YOUR CODE HERE
+  return 0 if arr.empty?
+  result = 0
+  arr.each do |elem|
+  	result += elem
+  end	
+  return result
 end
 
 def max_2_sum arr
-  # YOUR CODE HERE
+  return 0 if arr.empty?
+  max = arr.max(2)
+  result = 0
+  max.each do |i|
+  	result += i
+  end
+  result
 end
 
 def sum_to_n? arr, n
-  # YOUR CODE HERE
+  return false if arr.empty?
+	arr.combination(2).any? {|a, b| a + b == n }
 end
 
 # Part 2
 
 def hello(name)
-  # YOUR CODE HERE
+	"Hello, #{name}"
 end
 
 def starts_with_consonant? s
-  # YOUR CODE HERE
+	return false if s.empty?
+	/^[b-df-hj-np-tv-z]/i.match(s) != nil
 end
 
 def binary_multiple_of_4? s
-  # YOUR CODE HERE
+	return true if s == "0"
+	/^[10]*00$/.match(s) != nil
 end
 
 # Part 3
 
 class BookInStock
-# YOUR CODE HERE
+	attr_accessor :price
+	attr_accessor :isbn
+	def initialize(isbn,price)
+		raise ArgumentError, 
+			"Illegal argument" if isbn.empty?  or price <= 0
+		@price = price
+		@isbn = isbn
+	end
+	def price_as_string
+		sprintf("$%2.2f", @price)
+	end
 end
+
+#arr = [1,4,5,6]
+#puts sum(arr)
+
+#puts max_2_sum(arr)
+
+#puts arr.combination(2).to_a
+#puts starts_with_consonant?("abaradadfds")
