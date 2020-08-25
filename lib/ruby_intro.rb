54ecf8520c4047cf9ad5738bc3aeab15f1cbafb0
Hans Johnson <hans-johnson@uiowa.edu>
2020-08-25 11:53:13 -0500

    ENH: Allow IDE to track work to be done
    The "TODO:" string is recognized by many IDE's as
    a mechanism to generate a work list to be tracked and
    completed.

--- lib/ruby_intro.rb
+++ lib/ruby_intro.rb
@@ -4,33 +4,33 @@
 # Part 1
 
 def sum(arr)
-  # YOUR CODE HERE
+  # TODO: YOUR CODE HERE
 end
 
 def max_2_sum(arr)
-  # YOUR CODE HERE
+  # TODO: YOUR CODE HERE
 end
 
 def sum_to_n?(arr, n)
-  # YOUR CODE HERE
+  # TODO: YOUR CODE HERE
 end
 
 # Part 2
 
 def hello(name)
-  # YOUR CODE HERE
+  # TODO: YOUR CODE HERE
 end
 
 def starts_with_consonant?(s)
-  # YOUR CODE HERE
+  # TODO: YOUR CODE HERE
 end
 
 def binary_multiple_of_4?(s)
-  # YOUR CODE HERE
+  # TODO: YOUR CODE HERE
 end
 
 # Part 3
 
 class BookInStock
-# YOUR CODE HERE
+# TODO: YOUR CODE HERE
 end
