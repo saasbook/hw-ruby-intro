a77f0829ab20ecc4d347ce92bdb0def221248a51
cs169bot <cs169bot@users.noreply.github.com>
2021-04-06 01:43:04 +0000

    Automated Commit. CHIPS 2.5: hw-ruby-intro - This repository is created by automation from github workflows, found at https://github.com/saasbook/hw-ruby-intro-ci.

--- .github/CHIPS-config.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-chips_number="2.5"
-chips_name="hw-ruby-intro"
-ci_repo_link="https://github.com/saasbook/hw-ruby-intro-ci"
-not_ci_repo_ssh="git@github.com:saasbook/hw-ruby-intro.git"
-commit_message="Automated Commit. CHIPS ${chips_number}: ${chips_name} - This repository is created by automation from \
-github workflows, found at ${ci_repo_link}." 
-final_folder="^./root\(/.*\)?" 
